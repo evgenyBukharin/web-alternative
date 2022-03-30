9cc1376008c7d27e45d77394e4c72a3613bf61c6
Evgeniy <buharinzxc123@gmail.com>
2022-03-30 17:11:34 +0500

    clients about finish

--- src/js/_vars.js
+++ src/js/_vars.js
@@ -1,6 +1,8 @@
 export default {
-  windowEl: window,
-  documentEl: document,
-  htmlEl: document.documentElement,
-  bodyEl: document.body,
-}
+	windowEl: window,
+	documentEl: document,
+	htmlEl: document.documentElement,
+	bodyEl: document.body,
+	featuresLinks: document.querySelectorAll(".features__link"),
+	featuresImages: document.querySelectorAll(".features__image"),
+};
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,20 @@
-import './_vendor';
-import vars from './_vars';
-import './_functions';
-import './_components';
+import "./_vendor";
+import vars from "./_vars";
+import "./_functions";
+import "./_components";
+
+vars.featuresLinks.forEach((link) => {
+	link.addEventListener("click", () => {
+		vars.featuresLinks.forEach((l) => {
+			l.classList.remove("features__link-active");
+		});
+		link.classList.add("features__link-active");
+
+		vars.featuresImages.forEach((i) => {
+			i.classList.remove("features__image-active");
+		});
+		document
+			.getElementById(link.getAttribute("data-img"))
+			.classList.add("features__image-active");
+	});
+});
