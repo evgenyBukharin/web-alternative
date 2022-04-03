0181950e046f95c1108f078390debc650ceac4de
Александр Бухарин <buharinzxc123@gmail.com>
2022-04-03 20:38:28 +0500

    fixes

--- src/js/_vars.js
+++ src/js/_vars.js
@@ -5,4 +5,11 @@ export default {
 	bodyEl: document.body,
 	featuresLinks: document.querySelectorAll(".features__link"),
 	featuresImages: document.querySelectorAll(".features__image"),
+	clients: document.querySelectorAll(".clients__image"),
+	btnBackward: document.getElementById("btnBackward"),
+	btnForward: document.getElementById("btnForward"),
+	clients: document.querySelectorAll(".clients__image"),
+	popup: document.getElementById("popup"),
+	popupTrigger: document.getElementById("popupTrigger"),
+	popupClose: document.getElementById("popupClose"),
 };
--- src/js/main.js
+++ src/js/main.js
@@ -18,3 +18,56 @@ vars.featuresLinks.forEach((link) => {
 			.classList.add("features__image-active");
 	});
 });
+
+vars.btnBackward.addEventListener("click", () => {
+	let currNum = document
+		.querySelector(".clients__image-active")
+		.getAttribute("data-number");
+	if (currNum == 1) {
+		document
+			.getElementById("client1")
+			.classList.remove("clients__image-active");
+		document
+			.getElementById("client4")
+			.classList.add("clients__image-active");
+	} else {
+		document
+			.getElementById("client" + currNum)
+			.classList.remove("clients__image-active");
+		document
+			.getElementById("client" + (currNum - 1))
+			.classList.add("clients__image-active");
+	}
+});
+
+vars.btnForward.addEventListener("click", () => {
+	let currNum = document
+		.querySelector(".clients__image-active")
+		.getAttribute("data-number");
+	if (currNum == 4) {
+		document
+			.getElementById("client4")
+			.classList.remove("clients__image-active");
+		document
+			.getElementById("client1")
+			.classList.add("clients__image-active");
+	} else {
+		document
+			.getElementById("client" + currNum)
+			.classList.remove("clients__image-active");
+		currNum++;
+		document
+			.getElementById("client" + currNum)
+			.classList.add("clients__image-active");
+	}
+});
+
+vars.popupTrigger.addEventListener("click", () => {
+	vars.popup.classList.add("howWeWork__popup-visible");
+	document.body.style.overflow = "hidden";
+});
+
+vars.popupClose.addEventListener("click", () => {
+	vars.popup.classList.remove("howWeWork__popup-visible");
+	document.body.style.overflow = "unset";
+});
